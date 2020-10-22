299f2f54e23f7300c8fd0de02f4f20fc1263f2e3
yuanxiaoguo <1963895948@qq.com>
2020-10-22 19:55:07 +0800

    Initial commit

--- stduy1/shudu.py
+++ stduy1/shudu.py
@@ -1,3 +1,27 @@
+from aip import AipOcr
+# 百度api接口，读取图片文字
+
+APP_ID = '20401822'
+API_KEY = 'pzLvqEQplOKnCtGjBm7jUOHv'
+SECRET_KEY = 'lW65AdMwiNKORMrlVN5XBeYIPlj8ZLS8'
+
+
+client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+
+
+def get_file_content(file):
+    with open(file, 'rb') as fp:
+        return fp.read()
+
+def img_to_str(image_path):
+    image = get_file_content(image_path)
+    result = client.basicGeneral(image)
+    if 'words_result' in result:
+        return '\n'.join([w['words'] for w in result['words_result']])
+
+
+
+
 solustion=[[0,7,8,4,1,0,0,0,9],
            [2,0,1,0,2,0,4,7,0],
            [0,2,9,0,6,0,0,0,0],
